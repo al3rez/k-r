54bc7238065aa18d6402cea28be4648f293e1c5b
Alireza Bashiri <azbshiri@gmail.com>
2016-04-19 10:50:09 +0430

    Intial commit

---
+++ exercise-1-9/program.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+/* copy input to output and replace each one or
+ * more blanks by a single blank */
+ 
+main()
+{
+    int c;
+    int acc;
+ 
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' && acc == ' ')
+            acc == c;
+        else
+            putchar(c);
+ 
+        acc = c;
+    }
+ 
+}
